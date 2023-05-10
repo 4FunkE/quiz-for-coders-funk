7f43003b7c602374a5aa8955a2e2a64e593bed6d
4FunkE <4FunkEcodes@gmail.com>
2023-05-10 18:10:26 -0400

    psuedo commented with criteria

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+//click start button
 
+// timer starts
+
+// questions presentation 0,1,2,3,4; wrong answer subtract time&say wrong&next; right answer say correct show next
+
+//all questions answered or time 0 = all finished enter intitails 
